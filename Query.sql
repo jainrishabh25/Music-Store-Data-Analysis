6cae34457f568bb3f3e575d795aae795d1a22cb5
Rishabh Jain <109890124+jainrishabh25@users.noreply.github.com>
2023-12-03 20:31:05 +0530

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -86,6 +86,99 @@ where miliseconds > (
 )
 order by miliseconds desc;
 
+/* Question Set 3 - Advance */
+
+/* Q1: Find how much amount spent by each customer on artists? Write a query to return customer name, artist name and total spent */
+
+/* Steps to Solve: First, find which artist has earned the most according to the InvoiceLines. Now use this artist to find 
+which customer spent the most on this artist. For this query, you will need to use the Invoice, InvoiceLine, Track, Customer, 
+Album, and Artist tables. Note, this one is tricky because the Total spent in the Invoice table might not be on a single product, 
+so you need to use the InvoiceLine table to find out how many of each product was purchased, and then multiply this by the price
+for each artist. */
+
+/* Q2: Most popular music genre for each country */
+
+/* Method 1: Using CTE */
+
+with popular_genre as 
+(
+    select count(invoice_line.quantity) as purchases, customer.country, genre.name, genre.genre_id, 
+    row_number() over(partition by customer.country order by count(invoice_line.quantity) desc) as rowno 
+    from invoice_line 
+    join invoice on invoice.invoice_id = invoice_line.invoice_id
+    join customer on customer.customer_id = invoice.customer_id
+    join track on track.track_id = invoice_line.track_id
+    join genre on genre.genre_id = track.genre_id
+    group by 2,3,4
+    order by 2 asc, 1 desc
+)
+select * from popular_genre where rowno <= 1;
+
+
+/* Method 2: Using Recursive */
+
+with recursive
+    sales_per_country as(
+        select count(*) as purchases_per_genre, customer.country, genre.name, genre.genre_id
+        from invoice_line
+        join invoice on invoice.invoice_id = invoice_line.invoice_id
+        join customer on customer.customer_id = invoice.customer_id
+        join track on track.track_id = invoice_line.track_id
+        join genre on genre.genre_id = track.genre_id
+        group by 2,3,4
+        order by 2
+    ),
+    max_genre_per_country as (select max(purchases_per_genre) as max_genre_number, country
+        from sales_per_country
+        group by 2
+        order by 2)
+
+select sales_per_country.* 
+from sales_per_country
+join max_genre_per_country on sales_per_country.country = max_genre_per_country.country
+where sales_per_country.purchases_per_genre = max_genre_per_country.max_genre_number;
+
+
+/* Q3: Customer that has spent the most on music for each country */
+
+/* Method 1: using CTE */
+
+with customter_with_country as (
+        select customer.customer_id,first_name,last_name,billing_country,sum(total) as total_spending,
+        row_number() over(partition by billing_country order by sum(total) desc) as rowno 
+        from invoice
+        join customer on customer.customer_id = invoice.customer_id
+        group by 1,2,3,4
+        order by 4 asc,5 desc
+)
+select * from customter_with_country where rowno <= 1;
+
+
+/* Method 2: Using Recursive */
+
+with recursive 
+    customter_with_country as (
+        select customer.customer_id,first_name,last_name,billing_country,sum(total) as total_spending
+        from invoice
+        join customer on customer.customer_id = invoice.customer_id
+        group by 1,2,3,4
+        order by 2,3 desc
+    ),
+    country_max_spending as(
+        select billing_country,max(total_spending) as max_spending
+        from customter_with_country
+        group by billing_country
+    )
+
+select cc.billing_country, cc.total_spending, cc.first_name, cc.last_name, cc.customer_id
+from customter_with_country cc
+join country_max_spending ms
+on cc.billing_country = ms.billing_country
+where cc.total_spending = ms.max_spending
+order by 1;
+
+/* Thank You :) */
+
 
 
 
